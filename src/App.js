03506cfdc66c7b65761abcde04e842bdc6bdadba
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-14 15:03:26 -0500

    now with props in App.js I can now happily remove initialState.

--- src/App.js
+++ src/App.js
@@ -8,23 +8,7 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
 const App = (props) => {
-    console.log(props);
-    const state = {
-        additionalPrice: 0,
-        car: {
-            price: 26395,
-            name: '2019 Ford Mustang',
-            image:
-                'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-            features: [],
-        },
-        additionalFeatures: [
-            { id: 1, name: 'V-6 engine', price: 1500 },
-            { id: 2, name: 'Racing detail package', price: 1500 },
-            { id: 3, name: 'Premium sound system', price: 500 },
-            { id: 4, name: 'Rear spoiler', price: 250 },
-        ],
-    };
+    console.log(props.state);
 
     const removeFeature = (item) => {
         // dispatch an action here to remove an item
@@ -37,22 +21,32 @@ const App = (props) => {
     return (
         <div className="boxes">
             <div className="box">
-                <Header car={state.car} />
-                <AddedFeatures car={state.car} />
+                <Header car={props.state.car} />
+                <AddedFeatures car={props.state.car} />
             </div>
             <div className="box">
                 <AdditionalFeatures
-                    additionalFeatures={state.additionalFeatures}
+                    additionalFeatures={props.state.additionalFeatures}
                 />
                 <Total
-                    car={state.car}
-                    additionalPrice={state.additionalPrice}
+                    car={props.state.car}
+                    additionalPrice={props.state.additionalPrice}
                 />
             </div>
         </div>
     );
 };
 const mapStateToProps = (state) => {
-    return { additionalPrice: state.additionalPrice };
+    return {
+        state: state,
+        // additionalPrice: state.additionalPrice,
+        // car: {
+        //     price: state.car.price,
+        //     name: state.car.name,
+        //     image: state.car.image,
+        //     features: state.car.features,
+        // },
+        // additionalFeatures: state.additionalFeatures,
+    };
 };
 export default connect(mapStateToProps, {})(App);
