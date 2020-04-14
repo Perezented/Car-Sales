4d60932312289164c3d1c0da2a51a0bb171b4032
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-14 14:27:27 -0500

    Changed the name of reducers.js to index.js. Took out the initialState, renamed it to that too. Console.log in index did not return much though.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import rootReducer from './reducers';
+import { rootReducer } from './reducers';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(rootReducer);
+console.log('current state: ', store);
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>,
     rootElement
