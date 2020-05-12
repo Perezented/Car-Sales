757424eb0672727483e1b4af67888844a8ade1f9
Michael Perez <mperez6473@gmail.com>
2020-05-12 15:25:16 -0500

    Have App working how it was before the mSTP. All the props' info has been updated. Next is to get the total working.

--- src/App.js
+++ src/App.js
@@ -7,45 +7,21 @@ import Total from './components/Total';
 
 import { connect } from 'react-redux';
 
-const App = () => {
-    const state = {
-        additionalPrice: 0,
-        car: {
-            price: 26395,
-            name: '2019 Ford Mustang',
-            image:
-                'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-            features: [],
-        },
-        additionalFeatures: [
-            { id: 1, name: 'V-6 engine', price: 1500 },
-            { id: 2, name: 'Racing detail package', price: 1500 },
-            { id: 3, name: 'Premium sound system', price: 500 },
-            { id: 4, name: 'Rear spoiler', price: 250 },
-        ],
-    };
-
-    const removeFeature = (item) => {
-        // dispatch an action here to remove an item
-    };
-
-    const buyItem = (item) => {
-        // dipsatch an action here to add an item
-    };
-
+const App = (state) => {
+    console.log(state);
     return (
         <div className="boxes">
             <div className="box">
-                <Header car={state.car} />
-                <AddedFeatures car={state.car} />
+                <Header car={state.state.car} />
+                <AddedFeatures car={state.state.car} />
             </div>
             <div className="box">
                 <AdditionalFeatures
-                    additionalFeatures={state.additionalFeatures}
+                    additionalFeatures={state.state.additionalFeatures}
                 />
                 <Total
-                    car={state.car}
-                    additionalPrice={state.additionalPrice}
+                    car={state.state.car}
+                    additionalPrice={state.state.additionalPrice}
                 />
             </div>
         </div>
@@ -53,7 +29,8 @@ const App = () => {
 };
 
 const mapStateToProps = (state) => {
-    return { state };
+    console.log(state);
+    return state;
 };
 
 export default connect(mapStateToProps, {})(App);
